7716e13a08905bab1dc09be37b11d131b322915b
Sourav Khare <souravkhare34@gmail.com>
2022-10-15 18:33:40 +0530

    Checking if employee is full time ,part time or absent and calculating daily salary

---
+++ EmpWageUC3.java
@@ -0,0 +1,36 @@
+package com.empwage;
+
+public class EmpWageUC3 {
+    public static void main(String[] args) {
+
+        //  Variables
+        int Is_Present = 1;
+        int Is_PartTime = 2;
+        int EmpRatePerHour = 20;
+        int empHrs ;
+        int empWage ;
+
+
+        int max=2 , min=0 ;
+        int empCheck = (int)(Math.random()*(max-min+1)+min);
+
+        // checking conditions
+        if(empCheck == Is_Present) {
+            System.out.println("Employee is Present");
+            empHrs = 8;
+        }
+        else if(empCheck == Is_PartTime) {
+            System.out.println("Employee is PartTime");
+            empHrs = 4;
+        }
+        else {
+            System.out.println("Employee is Absent");
+            empHrs = 0;
+        }
+        //Computation
+        empWage = empHrs * EmpRatePerHour ;
+        System.out.println("Employee Wage = "+empWage);
+
+    }
+}
+
